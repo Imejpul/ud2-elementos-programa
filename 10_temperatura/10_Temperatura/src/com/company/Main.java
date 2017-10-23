89e74dcc2c697e1af46576600bd3f7c9af4ec44a
Unknown <ivanmejiapulido@gmail.com>
2017-10-23 21:19:51 +0200

    issue ejercicio 10, cambio a "double" y modificación fórmulas

--- 10_temperatura/10_Temperatura/src/com/company/Main.java
+++ 10_temperatura/10_Temperatura/src/com/company/Main.java
@@ -7,37 +7,33 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	// write your code here
+        // write your code here
 
-        int temp = 0;
+        double temp;
         char escala;
-        int fah = 0;
-        int kel = 0;
+        double fah;
+        double kel;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        System.out.println("Introducir tº en ºC");
+        temp = Double.parseDouble(br.readLine());
 
-
-            System.out.println("Introducir tº en ºC");
-            String aux = br.readLine();
-            temp = Integer.parseInt(aux);
-
-        do{
+        do {
 
             System.out.println("Introducir escala a convertir");
             escala = br.readLine().charAt(0);
 
+        } while (escala != 'f' && escala != 'k');
 
-        }while (escala != 'f' && escala != 'k');
+        if (escala == 'f') {
 
-        if (escala == 'f'){
-
-            fah = temp +32;
+            fah = ((1.8 * temp) + 32);
             System.out.println("La temperatura en ºF es: " + fah);
 
         } else {
 
-            kel = temp + 273;
+            kel = temp + 273.15;
             System.out.println("La temperatura en ºK es: " + kel);
         }
     }
