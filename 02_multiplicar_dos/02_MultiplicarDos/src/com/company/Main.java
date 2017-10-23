44ddf59ef9ec60ff3e37b13c63a858e9734659c8
Unknown <ivanmejiapulido@gmail.com>
2017-10-23 20:40:58 +0200

    issue ejercicios 2 al 4, modificados ejercicios 1 y 2 por carpetas mezcladas

--- 02_multiplicar_dos/02_MultiplicarDos/src/com/company/Main.java
+++ 02_multiplicar_dos/02_MultiplicarDos/src/com/company/Main.java
@@ -5,26 +5,25 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// Multiplicar dos valores introducidos por teclado
+        // Multiplicar dos valores introducidos por teclado
 
         //pedimos los valores
 
         System.out.println("Introducir primer valor");
 
-            Scanner sc1 = new Scanner(System.in);
-            int valorUno = sc1.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int valorUno = sc.nextInt();
 
         System.out.println("Introducir segundo valor");
 
-            Scanner sc2 = new Scanner(System.in);
-            int valorDos = sc2.nextInt();
+        int valorDos = sc.nextInt();
 
         //Hacemos la operación almacenando el resultado en una variable
 
-        int res = valorUno*valorDos;
+        int res = valorUno * valorDos;
 
         //Mostramos el resultado
 
-        System.out.println(valorUno +  " x " + valorDos + " = " + res);
+        System.out.println(valorUno + " x " + valorDos + " = " + res);
     }
 }
